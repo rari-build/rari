cacd3a1845fca7ade5053fcb15f27bc651c84fb4
Ryan Skinner <hello@ryanskinner.com>
2025-08-15 21:21:24 -0400

    refactor: clean up Vite config

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,8 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
-
-import { rari, rariRouter } from "rari";
-import { defineConfig } from "rolldown-vite";
+import tailwindcss from '@tailwindcss/vite'
+import { rari, rariRouter } from 'rari'
+import { defineConfig } from 'rolldown-vite'
 
 export default defineConfig({
   plugins: [rari(), rariRouter(), tailwindcss()],
-});
+})
