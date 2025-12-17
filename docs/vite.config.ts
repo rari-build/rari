db28b083451372a07b85343907283ba24855e622
Ryan Skinner <hello@ryanskinner.com>
2025-12-17 11:12:55 -0500

    chore(docs): add marked to external dependencies in vite config

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     rari({
       serverBuild: {
-        external: ['@shikijs/*'],
+        external: ['marked', '@shikijs/*'],
       },
     }),
     rariRouter(),
