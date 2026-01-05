903d370df6e753080470f7f4883dda04c69c6111
Ryan Skinner <hello@ryanskinner.com>
2026-01-05 01:16:43 -0500

    chore(rari): remove file extensions from mdx exports

--- packages/rari/src/mdx.ts
+++ packages/rari/src/mdx.ts
@@ -1,2 +1,2 @@
-export { createMDXClientReference, createMDXClientReferences } from './runtime/mdx-client-components.js'
-export type { MDXClientComponentConfig } from './runtime/mdx-client-components.js'
+export { createMDXClientReference, createMDXClientReferences } from './runtime/mdx-client-components'
+export type { MDXClientComponentConfig } from './runtime/mdx-client-components'
