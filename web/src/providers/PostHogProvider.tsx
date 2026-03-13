be322dcf54e7567c9600de7305bb743698d6bf71
Ryan Skinner <hello@ryanskinner.com>
2026-03-12 22:45:36 -0400

    chore(web): update PostHog UI host endpoint

--- web/src/providers/PostHogProvider.tsx
+++ web/src/providers/PostHogProvider.tsx
@@ -17,7 +17,7 @@ export function PostHogProvider({ children }: { children: ReactNode }) {
       if (!posthog.__loaded) {
         posthog.init(key, {
           api_host: host,
-          ui_host: 'https://us.i.posthog.com',
+          ui_host: 'https://us.posthog.com',
           person_profiles: 'always',
           capture_pageview: false,
           capture_pageleave: true,
