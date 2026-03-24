6f5d12393fccf9e86727d38ac482f51a1b2b43be
Ryan Skinner <hello@ryanskinner.com>
2026-03-24 11:38:30 -0400

    chore(deps): upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import { antfu } from '@antfu/eslint-config'
-import e18e from '@e18e/eslint-plugin'
 import deMorgan from 'eslint-plugin-de-morgan'
 import oxlint from 'eslint-plugin-oxlint'
 
@@ -25,6 +24,5 @@ export default antfu(
     },
   },
   deMorgan.configs.recommended,
-  e18e.configs.recommended,
   ...oxlint.buildFromOxlintConfigFile(`${import.meta.dirname}/vite.config.ts`),
 )
