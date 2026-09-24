0cdf82d290a3772cef0cadf0aa456308d0a109b7
Ryan Skinner <hello@ryanskinner.com>
2026-09-24 16:26:59 -0400

    refactor: remove unused environment type generation and clean up tsconfig includes

---
+++ web/src/vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite-plus/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_POSTHOG_HOST?: string
+  readonly VITE_POSTHOG_KEY?: string
+  readonly VITE_SENTRY_DSN?: string
+}
